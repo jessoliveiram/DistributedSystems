0df689c1d8b181d5770b55c4fdc0afd0acabc1ad
Jéssica Martins <jessica.moliveira@g.globo>
2021-08-01 23:40:41 -0300

    fix sending via pipe

--- first_experiment/pipes/pipes.py
+++ first_experiment/pipes/pipes.py
@@ -1,4 +1,6 @@
 import os
+import time
+from math import ceil
 from random import randrange
 
 
@@ -14,27 +16,34 @@ def number_genarator(n):
 
 def is_prime(n):
     sqrt_n = n**(1/2)
-    for i in range(2, sqrt_n):
+    for i in range(2, ceil(sqrt_n)):
         if n % i == 0:
-            return "Nao e primo!"
-    return "E primo"
+            return False
+    return True
 
 
 def write_numbers(write_fd, quantity):
     n = 1
-    write_opened = os.fdopen(write_fd, 'w')
-    for i in range(int(quantity)):
-        n = number_genarator(n)
-        text = str(n) + " "
-        
-        write_opened.write(text)
-    os.write(write_fd, str.encode("0"))
-    write_opened.close()
+    with open(write_fd, "w", 1) as f:
+        for i in range(quantity):
+            n = number_genarator(n)
+            f.write(str(n)+"\n")
+            print("Sent random number")
+            time.sleep(1)
+        f.write(str(0) + "\n")
+        print("End")
 
 
 def read_numbers(read_fd):
-    n = (os.read(read_fd, 20)).decode()
-    print("Number readed: " + n)
+    with open(read_fd, "r") as f:
+        while True:
+            n = f.read(1)
+            print("Number: " + n)
+            # if n == 0:
+            #     break
+            # if is_prime(int(n)):
+            #     return "Prime number"
+            # return "Not prime"
 
 
 def parent_child(quantity):
