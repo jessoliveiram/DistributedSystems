5e7aae2b1e67cb7385ade9829d3bf0dab94faee9
Thiago Machado <th.machado@g.globo>
2021-08-02 21:51:02 -0300

    Changes to send mensages by tcp socket between producer and consumer

--- first_experiment/sockets/consumer.py
+++ first_experiment/sockets/consumer.py
@@ -1,24 +1,32 @@
 import socket
 
+
 def is_prime_number(n):
     for i in range(2, n):
         if n % i == 0:
-            return False
-    
-    return True
-
+            return str(n) + ' is not prime.'
+    return str(n) + ' is prime.'
 
 
 if __name__ == '__main__':
-    print(is_prime_number(3))
-    print(is_prime_number(16))
-    print(is_prime_number(17))
     tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     origin = ('127.0.0.1', 5000)
     tcp.bind(origin)
     tcp.listen(1)
     while True:
-        print('Waitting connection...')
+        print('Waiting connection...')
         con, client = tcp.accept()
-        print('Connect with:', client)
+        print('Connected with:', client)
+        number = int(con.recv(1024).decode())
+
+        print('Receive:', number)
+        if number == 0:
+            message = 'Job done! Exiting...'
+            print(message)
+            con.send(message.encode())
+            con.close()
+            exit()
+        message = is_prime_number(number)
+        print('Sending: ', message)
+        con.send(message.encode())
         con.close()
--- first_experiment/sockets/producer.py
+++ first_experiment/sockets/producer.py
@@ -4,20 +4,30 @@ import random, socket, sys
 def generate_number():
     return random.randrange(100) + 1
 
+
 def send_numbers(quantity):
     n = 1
     for i in range(quantity):
         n = n + generate_number()
+        print('Sending: ', n)
         send_number(n)
-        print(n)        
+    print('Sending: ', 0)
+    send_number(0)
+
 
 def send_number(n):
     tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     destiny = ('127.0.0.1', 5000)
     tcp.connect(destiny)
-    tcp.close
+    tcp.send(str(n).encode())
+    response = tcp.recv(1024)
+
+    print('Receive: ', response.decode())
+    tcp.close()
+
 
 if __name__ == '__main__':
     args = sys.argv
+    if len(args) == 1:
+        print('The second arg must be the quantity of numbers')
     send_numbers(int(args[1]))
-    
