4f79328f3124c44590bedef13b36407f2783f4bd
Thiago Machado <th.machado@g.globo>
2021-08-25 02:32:33 -0300

    Implementing spinlocks in cpp.

---
+++ second_experiment/spinlocks/spinlocks.cpp
@@ -0,0 +1,60 @@
+#include <vector>
+#include <atomic>
+#include <iostream>
+#include <thread>
+#include <numeric>
+
+using namespace std;
+atomic_flag lock_sum = ATOMIC_FLAG_INIT;
+int total = 0;
+
+vector<signed char> generate_numbers_vector(int n)
+{
+    vector<signed char> numbers_vector;
+    srand(time(NULL));
+    for (int i =0; i< n; i++)
+    {
+        int rand_number = (rand() % 201) -100;
+        numbers_vector.push_back(rand_number);
+    }
+
+    return numbers_vector;
+}
+
+void thread_safe_sum_vector_elements(vector<signed char>::const_iterator begin, vector<signed char>::const_iterator end)
+{
+    int sub_total = accumulate(begin, end, 0);
+    while(lock_sum.test_and_set()){}
+    total += sub_total;
+    lock_sum.clear();
+}
+
+void parallel_sum(vector<signed char> numbers, int n_threads)
+{
+    int thread_piece = (int) numbers.size()/n_threads;
+    vector<thread> threads;
+    for(int i=0; i<n_threads; i++)
+    {
+        vector<int> sub_vector = {numbers.begin() + (i * thread_piece), numbers.begin() + (i * thread_piece)+thread_piece};
+        if(i== n_threads -1){
+            sub_vector = {numbers.begin() +i * thread_piece, numbers.end()};
+        }
+
+        threads.emplace_back(thread_safe_sum_vector_elements, 
+                             numbers.begin() + i * (thread_piece), 
+                             numbers.end() -(n_threads-i-1)*(thread_piece));
+    }
+    for(auto& th : threads)
+    {
+        th.join();
+    }
+
+}
+
+int main()
+{
+    vector<signed char> numbers = generate_numbers_vector(10);
+    parallel_sum(numbers, 5);
+    cout << total << endl;
+}
+
