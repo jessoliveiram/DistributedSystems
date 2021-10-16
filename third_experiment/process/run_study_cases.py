df5e9288237a306a13a88697a853e197b922b9a0
Jéssica Martins <jessica.moliveira@g.globo>
2021-10-15 21:25:09 -0300

    Add count of time

--- third_experiment/process/run_study_cases.py
+++ third_experiment/process/run_study_cases.py
@@ -1,5 +1,6 @@
 import os
 from multiprocessing import Process
+from timeit import default_timer as timer
 
 from third_experiment.process.evaluator import verify_result
 
@@ -16,25 +17,41 @@ def run_n_process(N, REPEAT, SLEEP):
 
 
 def study_case_basic():
+    print("Case Basic")
+    start = timer()
     run_n_process(2, 10, 2)
+    stop = timer()
+    print("N = " + "2" + " Time: " + str(stop - start))
     verify_result(2, 10)
 
 
 def study_case_test_1():
+    print("Case 1")
     for i in [2, 4, 8, 16, 32]:
+        start = timer()
         run_n_process(i, 10, 2)
+        stop = timer()
+        print("N = " + str(i) + " Time: " + str(stop - start))
         verify_result(i, 10)
 
 
 def study_case_test_2():
+    print("Case 2")
     for i in [2, 4, 8, 16, 32, 64]:
+        start = timer()
         run_n_process(i, 5, 1)
+        stop = timer()
+        print("N = " + str(i) + " Time: " + str(stop - start))
         verify_result(i, 5)
 
 
 def study_case_test_3():
+    print("Case 2")
     for i in [2, 4, 8, 16, 32, 64, 128]:
+        start = timer()
         run_n_process(i, 3, 0)
+        stop = timer()
+        print("N = " + str(i) + " Time: " + str(stop - start))
         verify_result(i, 3)
 
 
